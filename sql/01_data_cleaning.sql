cedbcb0963d7e5f2bac68e9aa064a8bfb1037c74
RoyAl175 <reivishobed175@gmail.com>
2025-12-14 18:54:04 +0530

    Added data validation and cleaning logic

--- sql/01_data_cleaning.sql
+++ sql/01_data_cleaning.sql
@@ -1,1 +1,80 @@
 -- Data cleaning and validation
+USE SwiggyDB;
+
+-- ============================
+-- DATA VALIDATION & CLEANING
+-- ============================
+
+-- Null Check
+SELECT
+    SUM(CASE WHEN State IS NULL THEN 1 ELSE 0 END) AS null_state,
+    SUM(CASE WHEN City IS NULL THEN 1 ELSE 0 END) AS null_city,
+    SUM(CASE WHEN Order_Date IS NULL THEN 1 ELSE 0 END) AS null_order_date,
+    SUM(CASE WHEN Restaurant_Name IS NULL THEN 1 ELSE 0 END) AS null_restaurant,
+    SUM(CASE WHEN Location IS NULL THEN 1 ELSE 0 END) AS null_location,
+    SUM(CASE WHEN Category IS NULL THEN 1 ELSE 0 END) AS null_category,
+    SUM(CASE WHEN Dish_Name IS NULL THEN 1 ELSE 0 END) AS null_dish,
+    SUM(CASE WHEN Price_INR IS NULL THEN 1 ELSE 0 END) AS null_price,
+    SUM(CASE WHEN Rating IS NULL THEN 1 ELSE 0 END) AS null_rating,
+    SUM(CASE WHEN Rating_Count IS NULL THEN 1 ELSE 0 END) AS null_rating_count
+FROM dbo.swiggy_data;
+
+-- Blank / Empty String Check
+SELECT *
+FROM dbo.swiggy_data
+WHERE
+    State = ''
+    OR City = ''
+    OR Restaurant_Name = ''
+    OR Location = ''
+    OR Category = ''
+    OR Dish_Name = '';
+
+-- Duplicate Detection
+SELECT
+    State,
+    City,
+    order_date,
+    restaurant_name,
+    location,
+    category,
+    dish_name,
+    price_INR,
+    rating,
+    rating_count,
+    COUNT(*) AS CNT
+FROM swiggy_data
+GROUP BY
+    State,
+    City,
+    order_date,
+    restaurant_name,
+    location,
+    category,
+    dish_name,
+    price_INR,
+    rating,
+    rating_count
+HAVING COUNT(*) > 1;
+
+-- Remove Duplicates
+WITH CTE AS (
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY
+                   State,
+                   City,
+                   order_date,
+                   restaurant_name,
+                   location,
+                   category,
+                   dish_name,
+                   price_INR,
+                   rating,
+                   rating_count
+               ORDER BY (SELECT NULL)
+           ) AS rn
+    FROM swiggy_data
+)
+DELETE FROM CTE
+WHERE rn > 1;
